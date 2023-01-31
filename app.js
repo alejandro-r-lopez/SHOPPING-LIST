921cc7c23e20f1e80270e5a056271e68b8935425
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-31 15:19:38 -0500

    displaying list

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let itemsData = [];
 window.addEventListener('load', async () => {
     itemsData = await getItems();
     console.log(itemsData);
+    displayItems();
 });
 
 createForm.addEventListener('submit', async (e) => {
@@ -18,3 +19,17 @@ createForm.addEventListener('submit', async (e) => {
     const newItem = new FormData(createForm);
     await createItem(newItem.get('item'), newItem.get('quantity'));
 });
+
+function displayItems() {
+    for (let item of itemsData) {
+        const itemBox = document.createElement('div');
+        const itemThing = document.createElement('p');
+        const itemQuantity = document.createElement('p');
+
+        itemThing.textContent = item.item;
+        itemQuantity.textContent = item.quantity;
+
+        itemBox.append(itemThing, itemQuantity);
+        listSection.append(itemBox);
+    }
+}
